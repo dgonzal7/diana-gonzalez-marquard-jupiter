eee63656f4f2dc3137e6e5566113b03b380ca57a
Diana <180865641+dgonzal7@users.noreply.github.com>
2025-01-15 20:16:04 -0500

    commit changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Add a footer 
 const footer = document.createElement("footer");
 const body = document.querySelector("body");
-body.appendChild(footer);
+document.body.appendChild(footer);
 
 // Create a variable named footer and assign it the footer element by using "DOM Selection" to select the <footer> element from the DOM
 
@@ -18,16 +18,8 @@ footer.appendChild(copyright);
 
 //Technical Skills
 const skills = ["JavaScript", "HTML", "CSS", "GitHub"];
-
 const skillsSection = document.querySelector('#Skills');
-
 const skillsList = skillsSection.querySelector("ul");
-
-// //for (let i = 0; i < skills.length; i++) {
-//   let skill = document.createElement("li");
-//   skill.innerText = skill;
-//   skillsList.appendChild(skill);
-// }
 
 for (let skill of skills) {
   let newskill = document.createElement("li");
