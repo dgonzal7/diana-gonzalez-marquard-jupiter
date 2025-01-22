94f2caf6d0c9b82f57af1075d5da44f9415d3d93
Diana <180865641+dgonzal7@users.noreply.github.com>
2025-01-21 22:08:41 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -26,3 +26,46 @@ for (let skill of skills) {
   newskill.innerHTML = skill;
   skillsList.appendChild(newskill);
 }
+
+const messageForm = document.querySelector("[name='leave_message']");
+messageForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e){
+  e.preventDefault();
+  const name = e.target.usersName.value;
+  const email = e.target.usersEmail.value;
+  const message = e.target.usersMessage.value;
+  console.log(name, email, message);
+  
+  //Messages in List
+  const messageSection = document.querySelector('#messages');
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+  
+  newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>\n
+  <span>${usersMessage}</span>`;
+  
+  //New Button Element
+  const removeButton = document.createElement("remove-button");
+  removeButton.innerText = "Remove";
+  removeButton.setAttribute("type", "button");
+  removeButton.setAttribute("id", "remove-buttonid");
+  //removeButton.addEventListener("click", handleRemove);
+  //const entry = document.querySelector("parentNode");
+  //remove(entry);
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  //Clear the form
+  messageForm.reset();
+};
+
+const messageSection = document.querySelector('#messages');
+const messageList = messageSection.querySelector("ul");
+//const newMessage = messageSection.createElement("li");
+// newMessage.innerHTML = ;
+//<a href="mailto:usersEmail">usersName</a>
+//<span>usersMessage</span>
+
+//const removeButton 
